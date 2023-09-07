b2b098bd32a51c40bc9068327118b1e5528df042
Brody Much <bamuch@students.ntc.edu>
2023-09-07 11:54:10 -0500

    added the NTCStudyBuddy .netMaui app

---
+++ NTCStudyBuddy/NTCStudyBuddy/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NTCStudyBuddy
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ NTCStudyBuddy/NTCStudyBuddy/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace NTCStudyBuddy
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
