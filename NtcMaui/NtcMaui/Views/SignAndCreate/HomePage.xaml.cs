6f86a1bcc6763731d65da0509e2bb1f64a021df9
Brody Much <bamuch@students.ntc.edu>
2023-09-19 13:27:42 -0500

    Maui log in and creation pretty much complete

--- NtcMaui/NtcMaui/Views/SignAndCreate/HomePage.xaml.cs
+++ NtcMaui/NtcMaui/Views/SignAndCreate/HomePage.xaml.cs
@@ -1,9 +1,20 @@
+using System.ComponentModel;
+using ApiStudyBuddy.Models;
+
 namespace NtcMaui.Views.SignAndCreate;
 
-public partial class HomePage : ContentPage
+public partial class HomePage : ContentPage, IQueryAttributable, INotifyPropertyChanged
 {
 	public HomePage()
 	{
 		InitializeComponent();
 	}
+
+    public void ApplyQueryAttributes(IDictionary<string, object> query)
+    {
+        LoggedInUser = query["Current User"] as User;
+        OnPropertyChanged("Current User");
+        UserNameText.Text = LoggedInUser.Username;
+    }
+    public User LoggedInUser { get; set; }
 }
