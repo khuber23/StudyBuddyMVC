b21de5e0b85cffb273d19945df7a29f7549db53e
Kayla <kehuber@students.ntc.edu>
2023-10-01 19:50:39 -0500

    #54 & #56 : Added the some views and UI for Study Session and My Studies

---
+++ StudyBuddyMVC/StudyBuddyMVC/Controllers/MyStudiesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudyBuddyMVC.Controllers
+{
+    public class MyStudiesController : Controller
+    {
+        [HttpGet("MyDashboard")]
+        [Route("MyDashboard")]
+        public IActionResult MyDashboard()
+        {
+            return View();
+        }
+
+        [HttpGet("Flashcards")]
+        [Route("Flashcards")]
+        public IActionResult Flashcards()
+        {
+            return View();
+        }
+
+        [HttpGet("Decks")]
+        [Route("Decks")]
+        public IActionResult Decks()
+        {
+            return View();
+        }
+
+        [HttpGet("DeckGroups")]
+        [Route("DeckGroups")]
+        public IActionResult DeckGroups()
+        {
+            return View();
+        }
+    }
+}
---
+++ StudyBuddyMVC/StudyBuddyMVC/Controllers/StudySessionController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudyBuddyMVC.Controllers
+{
+	public class StudySessionController : Controller
+	{
+		[HttpGet("MySession")]
+		[Route("MySession")]
+		public IActionResult MySession()
+		{
+			return View();
+		}
+
+		[HttpGet("StudyPriority")]
+		[Route("StudyPriority")]
+		public IActionResult StudyPriority()
+		{
+			return View();
+		}
+	}
+}
