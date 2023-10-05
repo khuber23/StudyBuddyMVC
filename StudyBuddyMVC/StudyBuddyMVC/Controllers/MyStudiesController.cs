a781e88d0af388b6675a254187b332c4fd3348a7
Kayla <kehuber@students.ntc.edu>
2023-10-04 23:44:34 -0500

    MyStudies Consuming API: #60 - Flashcards #64 - Decks #87 - DeckGroups MySesstion Consuming API: #88 = Display flashcards when starting Session

--- StudyBuddyMVC/StudyBuddyMVC/Controllers/MyStudiesController.cs
+++ StudyBuddyMVC/StudyBuddyMVC/Controllers/MyStudiesController.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApiStudyBuddy.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace StudyBuddyMVC.Controllers
 {
     public class MyStudiesController : Controller
     {
+        Uri baseAddress = new Uri("https://localhost:7025/api/");
+        private readonly HttpClient _client;
+
+        public MyStudiesController()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = baseAddress;
+        }
+
         [HttpGet("MyDashboard")]
         [Route("MyDashboard")]
         public IActionResult MyDashboard()
@@ -15,21 +26,45 @@ namespace StudyBuddyMVC.Controllers
         [Route("Flashcards")]
         public IActionResult Flashcards()
         {
-            return View();
+            List<FlashCard> flashcards = new List<FlashCard>();
+            HttpResponseMessage response = _client.GetAsync("https://localhost:7025/api/FlashCard").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                flashcards = JsonConvert.DeserializeObject<List<FlashCard>>(data);
+            }
+            return View(flashcards);
         }
 
         [HttpGet("Decks")]
         [Route("Decks")]
         public IActionResult Decks()
         {
-            return View();
+            List<Deck> decks = new List<Deck>();
+            HttpResponseMessage response = _client.GetAsync("https://localhost:7025/api/Deck").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                decks = JsonConvert.DeserializeObject<List<Deck>>(data);
+            }
+            return View(decks);
         }
 
         [HttpGet("DeckGroups")]
         [Route("DeckGroups")]
         public IActionResult DeckGroups()
         {
-            return View();
+            List<DeckGroup> deckgroups = new List<DeckGroup>();
+            HttpResponseMessage response = _client.GetAsync("https://localhost:7025/api/DeckGroup").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                deckgroups = JsonConvert.DeserializeObject<List<DeckGroup>>(data);
+            }
+            return View(deckgroups);
         }
     }
 }
--- StudyBuddyMVC/StudyBuddyMVC/Controllers/StudySessionController.cs
+++ StudyBuddyMVC/StudyBuddyMVC/Controllers/StudySessionController.cs
@@ -9,6 +9,15 @@ namespace StudyBuddyMVC.Controllers
 {
 	public class StudySessionController : Controller
 	{
+		Uri baseAddress = new Uri("https://localhost:7025/api/");
+		private readonly HttpClient _client;
+
+		public StudySessionController()
+		{
+			_client = new HttpClient();
+			_client.BaseAddress = baseAddress;
+		}
+
 		[HttpGet("MySession")]
 		[Route("MySession")]
 		public IActionResult MySession()
@@ -21,6 +30,21 @@ namespace StudyBuddyMVC.Controllers
 		public IActionResult StudyPriority()
 		{
 			return View();
+		}
+
+        [HttpGet("StartSession")]
+        [Route("StartSession")]
+        public IActionResult StartSession()
+		{
+			List<FlashCard> flashcards = new List<FlashCard>();
+			HttpResponseMessage response = _client.GetAsync("https://localhost:7025/api/FlashCard").Result;
+
+			if(response.IsSuccessStatusCode)
+			{
+				string data = response.Content.ReadAsStringAsync().Result;
+				flashcards = JsonConvert.DeserializeObject<List<FlashCard>>(data);
+			}
+			return View(flashcards);
 		}
 	}
 }
